e0948b8548a66e24aba1f1f768d1ee8f2756baa5
ZahraHosseiniFar <ZAHRA.HOSSEINI.FAR2000@gmail.com>
2022-11-19 23:28:45 +0330

    Initial commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+    class Projet
+    {
+        public static void Main(string[] args)
+        {
+        Console.WriteLine("enter age in days : ");
+        string a = Console.ReadLine();
+        int x = Convert.ToInt32(a);
+        age(x);
+        Console.ReadKey();
+        }
+        private static void age (int x)
+    {
+        int year, month, week, day;
+         year = x / 365;
+        x %= 365;
+         month = x / 30;
+        x %= 30;
+         week = x / 7;
+        x %= 7;
+         day = x;
+        Console.WriteLine("the year of birth is : " + year);
+        Console.WriteLine("the month of birth is : " + month);
+        Console.WriteLine("the week of birth is : " + week);
+        Console.WriteLine("the day of birth is : " + day);
+    }
+    }//برنامه ای بنویسید که سن فرد بر حسب روز گرفته و به سال و ماه و هفته و روز تبدیل کند
+
